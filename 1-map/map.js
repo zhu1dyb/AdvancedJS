8e12c7b2a1de254bb0e3de80951fe2c12886c9db
Иван <zhuldyb@MacBook-Air-Ivan.local>
2023-12-10 23:51:58 +0300

    HW-1

--- 1-map/map.js
+++ 1-map/map.js
@@ -1,6 +1,20 @@
-const arr = [
+const array = [
     {id:1, name: "Вася"},
-    {id:2, name: "Ваня"},
+    {id:2, name: "Коля"},
     {id:1, name: "Вася"}
 ]
-console.log(arr)
+
+const newSet = new Set()
+
+function addSet(arr) {
+    const setId = new Set();
+    for(const arrItem of arr) {
+        if(!setId.has(arrItem.id)) {
+            setId.add(arrItem.id)
+            newSet.add(arrItem)
+        }
+        }
+        return newSet
+    }
+    addSet(array)
+    console.log(newSet)
